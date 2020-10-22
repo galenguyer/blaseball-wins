a834027f494e5cd5fe31019c16059937f42357b2
Galen Guyer <galen@galenguyer.com>
2020-10-22 14:24:51 -0400

    parse data from chronicler mostly correctly

--- src/App.js
+++ src/App.js
@@ -27,39 +27,115 @@ function App() {
             </div>
         );
     const day = latestStreamData.value.games.sim.day;
-    const season = latestStreamData.value.games.sim.season;
 
-    return <div>gamedata got: {gameData.data.length}</div>;
+    // only add games up to the current day
+    let playedGames = [];
+    gameData.data.map((game) => {
+        if (game.endTime != null) playedGames.push(game.data);
+    });
 
-    var teams = [];
+    var teams = {};
+    var teamIdList = [];
     // eslint-disable-next-line
-    latestStreamData.value.games.schedule.map((game) => {
-        teams.push({
-            id: game.awayTeam,
-            name: game.awayTeamNickname,
-            fullName: game.awayTeamName,
-            effectiveDay: game.gameComplete ? day + 1 : day,
-            wins: latestStreamData.value.games.standings.wins[game.awayTeam],
+    playedGames.map((game) => {
+        // add the Away team to the Teams list if not already there
+        if (teams[game.awayTeam] == null) {
+            teamIdList.push(game.awayTeam);
+            teams[game.awayTeam] = {
+                id: game.awayTeam,
+                name: game.awayTeamNickname,
+                fullName: game.awayTeamName,
+                effectiveDay: 0,
+                realWins: 0,
+                realLosses: 0,
+                winsWhenWon: 0,
+                winsWhenLoss: 0,
+            };
+        }
+        // add the Home team to the Teams list if not already there
+        if (teams[game.homeTeam] == null) {
+            teamIdList.push(game.homeTeam);
+            teams[game.homeTeam] = {
+                id: game.homeTeam,
+                name: game.homeTeamNickname,
+                fullName: game.homeTeamName,
+                effectiveDay: 0,
+                realWins: 0,
+                realLosses: 0,
+                winsWhenWon: 0,
+                winsWhenLoss: 0,
+            };
+        }
+
+        // always increment the effectiveDay
+        teams[game.awayTeam].effectiveDay++;
+        teams[game.homeTeam].effectiveDay++;
+
+        game.outcomes.forEach((outcome) => {
+            // Sun 2 active
+            if (game.weather == 1) {
+                // Sun 2 acted on Away team
+                if (outcome.endsWith(game.awayTeamNickname)) {
+                    // Away team Won game
+                    if (game.awayScore > game.homeScore) teams[game.awayTeam].winsWhenWon++;
+                    // Away team Lost game
+                    else teams[game.awayTeam].winsWhenLoss++;
+                }
+                // Sun 2 acted on the Home team
+                else {
+                    // Away team Won game
+                    if (game.awayScore > game.homeScore) teams[game.homeTeam].winsWhenLoss++;
+                    // Away team Lost game
+                    else teams[game.homeTeam].winsWhenWon++;
+                }
+            }
+            // Black Hole active
+            else if (game.weather == 14) {
+                // Black Hole acted on Away team
+                if (outcome.endsWith(game.awayTeamNickname + "!")) {
+                    // Away team Won game
+                    if (game.awayScore > game.homeScore) teams[game.awayTeam].winsWhenWon--;
+                    // Away team Lost game
+                    else teams[game.awayTeam].winsWhenLoss--;
+                }
+                // Black Hole acted on the Home team
+                else {
+                    // Away team Won game
+                    if (game.awayScore > game.homeScore) teams[game.homeTeam].winsWhenLoss--;
+                    // Away team Lost game
+                    else teams[game.homeTeam].winsWhenWon--;
+                }
+            }
         });
-        teams.push({
-            id: game.homeTeam,
-            name: game.homeTeamNickname,
-            fullName: game.homeTeamName,
-            effectiveDay: game.gameComplete ? day + 1 : day,
-            wins: latestStreamData.value.games.standings.wins[game.homeTeam],
-            realWins:
-                (game.gameComplete ? day + 1 : day) - latestStreamData.value.games.standings.losses[game.awayTeam],
-        });
+
+        if (game.awayScore > game.homeScore) {
+            teams[game.awayTeam].realWins++;
+            teams[game.awayTeam].winsWhenWon++;
+            teams[game.awayTeam].netWins++;
+            teams[game.homeTeam].realLosses++;
+        } else {
+            teams[game.awayTeam].realLosses++;
+            teams[game.homeTeam].realWins++;
+            teams[game.homeTeam].winsWhenWon++;
+            teams[game.homeTeam].netWins++;
+        }
     });
 
-    teams.sort(function (a, b) {
-        return b.wins / b.realWins - a.wins / a.realWins;
+    let teamList = [];
+    teamIdList.forEach((id) => {
+        teamList.push(teams[id]);
     });
 
-    const data = teams.map((team) => {
+    teamList.sort(function (a, b) {
+        return (b.winsWhenLoss + b.winsWhenWon) / b.realWins - (a.winsWhenLoss + a.winsWhenWon) / a.realWins;
+    });
+
+    console.log(teamList);
+
+    const data = teamList.map((team) => {
         return {
             name: team.name,
-            ratio: Math.floor((team.wins * 100) / team.realWins) / 100,
+            ratio: Math.floor(((team.winsWhenLoss + team.winsWhenWon) * 100) / team.realWins) / 100,
         };
     });
 
@@ -70,8 +146,8 @@ function App() {
             <br />
             <div className="Team">Format: [Team Name] - [Wins]:[Real Wins]</div>
             <br />
-            {teams.map((team) => (
-                <Team name={team.fullName} wins={team.wins} realWins={team.realWins} />
+            {teamList.map((team) => (
+                <Team name={team.fullName} wins={team.winsWhenLoss + team.winsWhenWon} realWins={team.realWins} />
             ))}
             <br />
             <div className="Comment" style={{ marginBottom: "1rem" }}>
